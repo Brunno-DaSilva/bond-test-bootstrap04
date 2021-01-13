ce68e3d885159d0804098b0dfe75bf4a0377a906
Bruno DaSilva <hi.dasilvab@gmail.com>
2021-01-13 14:38:12 -0600

    Added fetch and created an init function to generate whenever the fetch is called

---
+++ Fetch_BootstrapTest/JS/app.js
@@ -0,0 +1,520 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Variables
+
+  const searchInput = document.querySelector(".search");
+  const cardsContainer = document.querySelector(".cards-container");
+  const btnForm = document.getElementById("btn-form");
+  const btnPlus = document.getElementById("btn-plus");
+  const filtersToShow = document.getElementById("filtersToShow");
+  const arrow = document.getElementById("arrow");
+  const btnCategory = document.getElementById("btn-category");
+  const btnBudget = document.getElementById("btn-budget");
+  const btnSchool = document.getElementById("btn-school");
+  const btnProject = document.getElementById("btn-project");
+  const btnDate = document.getElementById("btn-date");
+  const btnCompletion = document.getElementById("btn-completion");
+  const schoolFilter = document.getElementById("dataType");
+  const categoryFilter = document.getElementById("projectCategory");
+
+  //API
+  const baseURL =
+    "https://res.friscoisd.org/services/Bond/Project?filterType=test&filter=test&refresh=true";
+
+  const dataSet = [];
+
+  const NO_DATA_FOUND = `<div class="notFound">
+  <div class="notFound__img">
+    <img src="https://res.cloudinary.com/duprwuo4j/image/upload/v1608312445/fisd-logo-full-color-rgb_mqunwz.png" alt="Search Icons">
+  </div>
+  <div class="notFound__text">
+    <h3>No results containing all your search terms were found.</h3>
+    <p>Your search: <span>${this.value}</span></p>
+    <p>Try searching for title, category, or campus type</p>
+  </div>
+</div>`;
+
+  fetch(baseURL)
+    .then((response) => response.json())
+    .then((data) => {
+      dataSet.push(...data);
+      init();
+    })
+    .catch((error) => {
+      cardsContainer.innerHTML = `${NO_DATA_FOUND} ${error}`;
+    });
+
+  function init() {
+    // Display data
+    const htmlData = ({
+      ThumbnailImg,
+      ProjectTitle,
+      ProjectedCompletionString,
+      ProjectedBudget,
+      PercentComplete,
+      Description,
+      IsComplete,
+      ProjectId,
+    }) => {
+      return `
+      <div class="searched-items">
+        <div class="items-head">
+          <div class="img-container">
+            <img src=${ThumbnailImg} alt=${ProjectTitle} />
+            ${
+              IsComplete
+                ? `<div class="img-container__label">
+                      <p>Completed</p>
+                   </div>`
+                : `""`
+            }
+  
+          </div>
+          <div class="item-wrapper">
+            <div class="items-short-info">
+              <div class="short-info-holder">
+                <i class="fas fa-calendar-alt"></i>
+                <span>${ProjectedCompletionString}</span>
+              </div>
+              <div class="short-info-holder">
+                <i class="fas fa-coins"></i>
+                <span>$${
+                  ProjectedBudget >= 1000000
+                    ? ProjectedBudget.toFixed(2).slice(0, 2)
+                    : ProjectedBudget.toFixed(2).slice(0, 3)
+                } 
+                ${ProjectedBudget >= 1000000 ? "M" : "K"}
+                </span>
+              </div>
+    
+              <div class="short-info-holders progress-bar">
+                <div class="progress-bar-value">
+                  ${PercentComplete}% 
+                </div>
+                <div class="progress-bar-fill"></div>
+              </div>
+    
+            </div>
+    
+            <h2 class="item-wrapper-title">
+              ${ProjectTitle}
+            </h2>
+          </div>
+        </div>
+        <div class="items__body">
+          <div class="item__text-wrapper">
+            <p class="item__text">${Description}</p>
+          </div>
+          <div class="item__btn-container">
+            <button type="button">
+              <a class="anchor-override" href="https://res.friscoisd.org/services/Bond/Updates?projectId=${ProjectId}&refresh=true">
+              Details
+              </a>
+            </button>
+          </div>
+        </div>
+      </div>`;
+    };
+
+    //Search
+    const findMatches = (wordToMatch, dataSet) => {
+      return dataSet.filter((data) => {
+        //regex
+        const regex = new RegExp(wordToMatch, "gi");
+
+        return (
+          data.ProjectTitle.match(regex) ||
+          data.CampusType.match(regex) ||
+          data.Category.match(regex)
+        );
+      });
+    };
+
+    function displayMatches() {
+      const matchArray = findMatches(this.value, dataSet);
+
+      const html = matchArray
+        .map((data) => {
+          return htmlData(data);
+        })
+        .join("");
+
+      if (html.length > 0) {
+        cardsContainer.innerHTML = html;
+      } else {
+        cardsContainer.innerHTML = `${NO_DATA_FOUND}`;
+      }
+    }
+
+    // DropDownFilter
+    function selectDropdown() {
+      const dataType = document.getElementById("dataType");
+      const projectCategory = document.getElementById("projectCategory");
+
+      const projectValue =
+        projectCategory.options[projectCategory.selectedIndex].value;
+      const selectedValue = dataType.options[dataType.selectedIndex].value;
+
+      if (selectedValue == "selectAll") {
+        if (projectValue == "selectAll") {
+          const html2 = dataSet
+            .map((data) => {
+              return htmlData(data);
+            })
+            .join("");
+
+          if (html2.length > 0) {
+            cardsContainer.innerHTML = html2;
+          } else {
+            cardsContainer.innerHTML = NO_DATA_FOUND;
+          }
+        } else if (projectValue == "newBuildings") {
+          const html6 = dataSet
+            .map((data) => {
+              if (data.Category.match(/\b(\w*New\w*)\b/g)) {
+                return htmlData(data);
+              }
+            })
+            .join("");
+
+          if (html6.length > 0) {
+            cardsContainer.innerHTML = html6;
+          } else {
+            cardsContainer.innerHTML = NO_DATA_FOUND;
+          }
+        } else if (projectValue == "athletics") {
+          const html6 = dataSet
+            .map((data) => {
+              if (data.Category.match(/\b(\w*Ath\w*)\b/g)) {
+                return htmlData(data);
+              }
+            })
+            .join("");
+
+          if (html6.length > 0) {
+            cardsContainer.innerHTML = html6;
+          } else {
+            cardsContainer.innerHTML = NO_DATA_FOUND;
+          }
+        } else if (projectValue == "fineArts") {
+          const html6 = dataSet
+            .map((data) => {
+              if (data.Category.match(/\b(\w*Fine\w*)\b/g)) {
+                return htmlData(data);
+              }
+            })
+            .join("");
+
+          if (html6.length > 0) {
+            cardsContainer.innerHTML = html6;
+          } else {
+            cardsContainer.innerHTML = NO_DATA_FOUND;
+          }
+        } else if (projectValue == "transportation") {
+          const html6 = dataSet
+            .map((data) => {
+              if (data.Category.match(/\b(\w*Transport\w*)\b/g)) {
+                return htmlData(data);
+              }
+            })
+            .join("");
+
+          if (html6.length > 0) {
+            cardsContainer.innerHTML = html6;
+          } else {
+            cardsContainer.innerHTML = NO_DATA_FOUND;
+          }
+        } else if (projectValue == "repairMaintenance") {
+          const html6 = dataSet
+            .map((data) => {
+              if (data.Category.match(/\b(\w*Repair\w*)\b/g)) {
+                return htmlData(data);
+              }
+            })
+            .join("");
+
+          if (html6.length > 0) {
+            cardsContainer.innerHTML = html6;
+          } else {
+            cardsContainer.innerHTML = NO_DATA_FOUND;
+          }
+        } else if (projectValue == "refreshRenew") {
+          const html6 = dataSet
+            .map((data) => {
+              if (data.Category.match(/\b(\w*Refresh\w*)\b/g)) {
+                return htmlData(data);
+              }
+            })
+            .join("");
+
+          if (html6.length > 0) {
+            cardsContainer.innerHTML = html6;
+          } else {
+            cardsContainer.innerHTML = NO_DATA_FOUND;
+          }
+        } else if (projectValue == "playground") {
+          const html6 = dataSet
+            .map((data) => {
+              if (data.Category.match(/\b(\w*Playground\w*)\b/g)) {
+                return htmlData(data);
+              }
+            })
+            .join("");
+
+          if (html6.length > 0) {
+            cardsContainer.innerHTML = html6;
+          } else {
+            cardsContainer.innerHTML = NO_DATA_FOUND;
+          }
+        } else if (projectValue == "FF&E") {
+          const html6 = dataSet
+            .map((data) => {
+              if (data.Category.match(/\b(\w*FF&E\w*)\b/g)) {
+                return htmlData(data);
+              }
+            })
+            .join("");
+
+          if (html6.length > 0) {
+            cardsContainer.innerHTML = html6;
+          } else {
+            cardsContainer.innerHTML = NO_DATA_FOUND;
+          }
+        } else if (projectValue == "security") {
+          const html6 = dataSet
+            .map((data) => {
+              if (data.Category.match(/\b(\w*Security\w*)\b/g)) {
+                return htmlData(data);
+              }
+            })
+            .join("");
+
+          if (html6.length > 0) {
+            cardsContainer.innerHTML = html6;
+          } else {
+            cardsContainer.innerHTML = NO_DATA_FOUND;
+          }
+        } else if (projectValue == "technology") {
+          const html6 = dataSet
+            .map((data) => {
+              if (data.Category.match(/\b(\w*Technology\w*)\b/g) != null) {
+                return htmlData(data);
+              }
+            })
+            .join("");
+
+          if (html6.length > 0) {
+            cardsContainer.innerHTML = html6;
+          } else {
+            cardsContainer.innerHTML = NO_DATA_FOUND;
+          }
+
+          return;
+        } else if (projectValue == "other") {
+          const html6 = dataSet
+            .map((data) => {
+              if (data.Category.match(/\b(\w*Other\w*)\b/g)) {
+                return htmlData(data);
+              }
+            })
+            .join("");
+
+          if (html6.length > 0) {
+            cardsContainer.innerHTML = html6;
+          } else {
+            cardsContainer.innerHTML = NO_DATA_FOUND;
+          }
+        } else {
+          const msg = `<div><h1>Search Not found here are some results</h1></div>`;
+          const html2 = dataSet
+            .map((data) => {
+              return htmlData(data);
+            })
+            .join("");
+          cardsContainer.innerHTML = msg + html2;
+        }
+      } else if (selectedValue == "highSchool") {
+        const html3 = dataSet
+          .map((data) => {
+            if (data.CampusType.match(/\b(\w*High\w*)\b/g)) {
+              return htmlData(data);
+            }
+          })
+          .join("");
+
+        if (html3.length > 0) {
+          cardsContainer.innerHTML = html3;
+        } else {
+          cardsContainer.innerHTML = NO_DATA_FOUND;
+        }
+      } else if (selectedValue == "elementarySchool") {
+        const html3 = dataSet
+          .map((data) => {
+            if (data.CampusType.match(/\b(\w*Elementary\w*)\b/g)) {
+              return htmlData(data);
+            }
+          })
+          .join("");
+
+        if (html3.length > 0) {
+          cardsContainer.innerHTML = html3;
+        } else {
+          cardsContainer.innerHTML = NO_DATA_FOUND;
+        }
+      } else if (selectedValue == "middleSchool") {
+        const html3 = dataSet
+          .map((data) => {
+            if (data.CampusType.match(/\b(\w*Middle\w*)\b/gi)) {
+              return htmlData(data);
+            }
+          })
+          .join("");
+
+        if (html3.length > 0) {
+          cardsContainer.innerHTML = html3;
+        } else {
+          cardsContainer.innerHTML = NO_DATA_FOUND;
+        }
+      } else if (selectedValue == "other") {
+        const html3 = dataSet
+          .map((data) => {
+            if (data.CampusType.match(/\b(\w*Other\w*)\b/gi)) {
+              return htmlData(data);
+            }
+          })
+          .join("");
+        if (html3.length > 0) {
+          cardsContainer.innerHTML = html3;
+        } else {
+          cardsContainer.innerHTML = NO_DATA_FOUND;
+        }
+      } else {
+        return NO_DATA_FOUND;
+      }
+    }
+
+    const showMoreFilters = () => {
+      if (filtersToShow.style.display === "flex") {
+        filtersToShow.style.display = "none";
+        arrow.style.display = "none";
+      } else {
+        filtersToShow.style.display = "flex";
+        arrow.style.display = "block";
+      }
+    };
+
+    //sorting Ascending Order
+
+    btnBudget.addEventListener("click", function () {
+      let sortedArr = dataSet.sort((a, b) => {
+        return a.projectBudget - b.projectBudget;
+      });
+      const html2 = sortedArr
+        .map((data) => {
+          return htmlData(data);
+        })
+        .join("");
+      cardsContainer.innerHTML = html2;
+    });
+
+    btnCategory.addEventListener("click", function () {
+      let sortedArr = dataSet.sort((a, b) => {
+        if (a.category < b.category) {
+          return -1;
+        }
+        if (a.category > b.category) {
+          return 1;
+        }
+        return 0;
+      });
+      const html2 = sortedArr
+        .map((data) => {
+          return htmlData(data);
+        })
+        .join("");
+      cardsContainer.innerHTML = html2;
+    });
+
+    btnSchool.addEventListener("click", function () {
+      let sortedArr = dataSet.sort((a, b) => {
+        if (a.title < b.title) {
+          return -1;
+        }
+        if (a.title > b.title) {
+          return 1;
+        }
+        return 0;
+      });
+      const html2 = sortedArr
+        .map((data) => {
+          return htmlData(data);
+        })
+        .join("");
+      cardsContainer.innerHTML = html2;
+    });
+
+    btnProject.addEventListener("click", function () {
+      let sortedArr = dataSet.sort((a, b) => {
+        if (a.CampusType < b.CampusType) {
+          return -1;
+        }
+        if (a.CampusType > b.CampusType) {
+          return 1;
+        }
+        return 0;
+      });
+
+      const html2 = sortedArr
+        .map((data) => {
+          return htmlData(data);
+        })
+        .join("");
+      cardsContainer.innerHTML = html2;
+    });
+
+    btnDate.addEventListener("click", function () {
+      let sortedArr = dataSet.sort((a, b) => {
+        if (a.projectCompletion < b.projectCompletion) {
+          return -1;
+        }
+        if (a.projectCompletion > b.projectCompletion) {
+          return 1;
+        }
+        return 0;
+      });
+
+      const html2 = sortedArr
+        .map((data) => {
+          return htmlData(data);
+        })
+        .join("");
+      cardsContainer.innerHTML = html2;
+    });
+
+    btnCompletion.addEventListener("click", function () {
+      let sortedArr = dataSet.sort((a, b) => {
+        if (a.completionPercent < b.completionPercent) {
+          return -1;
+        }
+        if (a.completionPercent > b.completionPercent) {
+          return 1;
+        }
+        return 0;
+      });
+
+      const html2 = sortedArr
+        .map((data) => {
+          return htmlData(data);
+        })
+        .join("");
+      cardsContainer.innerHTML = html2;
+    });
+
+    btnPlus.addEventListener("click", showMoreFilters);
+    btnForm.addEventListener("click", selectDropdown);
+
+    categoryFilter.addEventListener("change", selectDropdown);
+    schoolFilter.addEventListener("change", selectDropdown);
+
+    searchInput.addEventListener("keyup", displayMatches);
+  }
+});
---
+++ Fetch_BootstrapTest/JS/form-style.js
@@ -0,0 +1,1 @@
+console.log("ciao bela me piacere parlare italiano");
