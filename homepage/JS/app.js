04987dfa8eae1b714ce2464218ca2f6ad2f46e74
Bruno DaSilva <hi.dasilvab@gmail.com>
2021-02-01 15:09:16 -0600

    Pushed data

--- homepage/JS/app.js
+++ homepage/JS/app.js
@@ -1,8 +1,16 @@
+/*===================
+==>  Variables
+===================*/
+
 const progress = document.querySelector(".progress-bar__completed");
 const percentage_completion = document.getElementById("percentage_completion");
-
 const data = 70;
 const percentage_span = `<span>${data} Completion</span>`;
+
+/*=====================
+==>  DOM Manipulation
+=====================*/
+
 percentage_completion.innerHTML = `${percentage_span}`;
 progress.style.width = `${data}%`;
 progress.style.opacity = 1;
