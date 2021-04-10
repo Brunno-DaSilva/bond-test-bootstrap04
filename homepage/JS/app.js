517ad76c5f28ab15bf5b726cf633695c41e546bc
Bruno DaSilva <hi.dasilvab@gmail.com>
2021-04-09 20:08:54 -0500

    Pushing fetched poercentage completion so the use can dynamically change it in spreasheet

--- homepage/JS/app.js
+++ homepage/JS/app.js
@@ -4,13 +4,28 @@
 
 const progress = document.querySelector(".progress-bar__completed");
 const percentage_completion = document.getElementById("percentage_completion");
+const BASE_URL = `https://res.friscoisd.org/services/Bond/ProjectCompletion?refresh=true`;
 const data = 70;
 const percentage_span = `<span>${data} Completion</span>`;
+const dataSet = [];
 
 /*=====================
 ==>  DOM Manipulation
 =====================*/
 
-percentage_completion.innerHTML = `${percentage_span}`;
-progress.style.width = `${data}%`;
-progress.style.opacity = 1;
+function getData(url) {
+  fetch(url)
+    .then((res) => {
+      return res.json();
+    })
+    .then((jsonData) => {
+      percentage_completion.innerHTML = `${jsonData.CompletionPercentage}`;
+      progress.style.width = `${jsonData.CompletionPercentage}%`;
+      progress.style.opacity = 1;
+    })
+    .catch((error) => {
+      console.log("This is an error", error);
+    });
+}
+
+getData(BASE_URL);
